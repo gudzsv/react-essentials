a3d92b302112814988c6871e639c0a6f1541b23a
gudzsv <gudzsvmail@gmail.com>
2024-07-18 04:35:32 +0300

    added props

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import reactImg from './assets/react-core-concepts.png';
+import componentsImg from './assets/components.png';
 
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
 function gerRandomInt(max) {
@@ -20,12 +21,46 @@ function Header() {
 	);
 }
 
+function CoreConcept(props) {
+	return (
+		<li>
+			<img src={props.image} alt={props.title} />
+			<h3>{props.title}</h3>
+			<p>{props.description}</p>
+		</li>
+	);
+}
+
 function App() {
 	return (
 		<div>
 			<Header />
 			<main>
-				<h2>Time to get started!</h2>
+				<section id='core-concepts'>
+					<h2>Core Concepts</h2>
+					<ul>
+						<CoreConcept
+							title='Components'
+							description='The Core UI building block.'
+							image={componentsImg}
+						/>
+						<CoreConcept
+							title='Props'
+							description='The Core UI building block.'
+							image={componentsImg}
+						/>
+						<CoreConcept
+							title='Components'
+							description='The Core UI building block.'
+							image={componentsImg}
+						/>
+						<CoreConcept
+							title='Components'
+							description='The Core UI building block.'
+							image={componentsImg}
+						/>
+					</ul>
+				</section>
 			</main>
 		</div>
 	);
